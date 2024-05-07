c3a40c0a674efbf26ae25a93b5523f59ff6678be
Nicolás Gómez García <nicogomezgarcia1@gmail.com>
2024-05-07 10:36:44 +0200

    falta vistas de formularios

--- TFG/resources/views/layouts/plantilla.blade.php
+++ TFG/resources/views/layouts/plantilla.blade.php
@@ -84,15 +84,23 @@
             </div>
         </nav>
     </header>
+     <div class="container">
     <main>
-        @yield('content')
+       
+            @yield('content')
+      
     </main>
-    <footer class="footer mt-auto py-3 ">
+  </div>
+    <footer class="footer mt-auto py-3">
         <div class="container">
-            <span class="text-muted">© 2024 102turno</span>
+            <div class="row">
+                <div class="col-md-4 d-flex align-items-center">
+                    <span class="text-muted">© 2024 102turno</span>
+                </div>
+            </div>
         </div>
     </footer>
-
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
